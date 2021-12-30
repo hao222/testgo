d5b60f1e6d72787bf42c911f688dd8dc4733598b
wuqilong <1154714871@qq.com>
2021-12-30 23:35:14 +0800

    2

---
+++ home_store/struct/inherit/inherit.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// go中的继承实现  一个struct嵌套了另一个匿名结构体，那么这个结构体可以直接访问匿名结构体的字段和方法， 从而实现了继承特性
+
+type Stu struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+func (stu *Stu) ShowInfo() {
+	fmt.Printf("%v--%v---%v", stu.Name, stu.Age, stu.Score)
+}
+
+// 继承自Stu 可以使用Stu的showinfo 方法
+type Puplie struct {
+	Stu
+}
+
+func (p *Puplie) tesing() {
+	fmt.Println("这是Puplie的私有方法")
+}
+
+func main()  {
+
+	var pp = Puplie{}
+	pp.Stu.Name = "xxx"
+	pp.Stu.Age = 12
+	pp.Score = 121
+	pp.tesing()
+	pp.Stu.ShowInfo()
+
+}
--- home_store/struct/main.go
+++ home_store/struct/main.go
@@ -12,8 +12,25 @@ type Stu struct {
 }
 type S Stu
 
-// struct 的每个字段上， 可以写上一个tag``. 该tag可以通过反射机制获取，  常用于json序列化 反序列化
+type student struct {
+	Name string
+	score float64
+}
 
+// 工厂模式  如果属性时小写 也不能在其他包直接获取， 可以定义一个方法获取
+func Factory(n string ,s float64) *student {
+	return &student{
+		Name: n,
+		score: s,
+	}
+}
+// 也是一种封装  设置set get方法
+func (s *student) GetScore() float64 {
+	return s.score
+}
+
+// struct 的每个字段上， 可以写上一个tag``. 该tag可以通过反射机制获取，  常用于json序列化 反序列化
+// 抽象  将具体的事务的属性描述到公共的字段上表示 代表一类的事物
 func main() {
 	var s1 Stu
 	var s2 S
@@ -24,5 +41,16 @@ func main() {
 	if err != nil {
 		fmt.Println("json处理错误", err)
 	}
-	fmt.Println("-------", s2, s1, string(res))
+
+	// 在创建结构体变量时， 把字段名和字段值写在一起， 这种写法， 就不依赖字段的定义顺序
+	var s3 = Stu{
+		Name: "xiaoming",
+		Age:  "12",
+	}
+	s5 := &Stu{"xxxx", "34"}
+	fmt.Println(*s5)
+	fmt.Println("-------", s2, s1, string(res), s3)
+	// 定student结构体首字母小写的时候， 让其他包能导入 除了首字母大写之外， 通过工厂模式写
+	var stu = student{"toms", 22.2}
+	fmt.Println(stu.Name, stu.GetScore())
 }
